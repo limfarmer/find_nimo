cbc57b6ce78591ec23b5f46d8f616d1ca29dafb3
user1 <98tmddyddl@gmail.com>
2024-04-04 16:03:35 +0900

    insert연결

--- src/main/java/com/nimo/jdbc/controller/ParcelController.java
+++ src/main/java/com/nimo/jdbc/controller/ParcelController.java
@@ -1,11 +1,11 @@
 package com.nimo.jdbc.controller;
 
 import com.nimo.jdbc.dao.ParcelDao;
+import com.nimo.jdbc.vo.AccountVo;
 import com.nimo.jdbc.vo.ParcelVo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -13,13 +13,40 @@ import java.util.List;
 @Controller
 @RequestMapping("/parcel")
 public class ParcelController {
-    @GetMapping("/pboard")
-    public String selectParcel(Model model, HttpSession session){
+    @GetMapping("/pboard") //list
+    public String selectParcel(Model model, HttpSession session) {
         ParcelDao parcelDao = new ParcelDao();
         List<ParcelVo> plist = parcelDao.parcelSelect();
         String id = (String) session.getAttribute("id");
-        model.addAttribute("plist",plist);
-        model.addAttribute("id",id);
+        model.addAttribute("plist", plist);
+        model.addAttribute("id", id);
         return "thymeleaf/pboard";
+    }
+
+    @GetMapping("/parcel") //detailpage
+    public String parcelMain(Model model, @RequestParam("pno") int pnum) {
+
+        ParcelDao parcelDao = new ParcelDao();
+        ParcelVo parcelMain = parcelDao.parcelMain(pnum);
+        model.addAttribute("parcelmain", parcelMain);
+        return "thymeleaf/pmain";
+    }
+
+    //    @GetMapping("/")
+    @GetMapping("/parcelInsert")
+    public String parcelInsert(Model model) {
+        model.addAttribute("pinsert",new ParcelVo ());
+        return "thymeleaf/pinsert";
+    }
+    @PostMapping("/parcelinsertt")
+    public String parcelInsertt(@ModelAttribute("pinsert") ParcelVo pinvo, HttpSession session){
+        ParcelDao parcelDao = new ParcelDao();
+        String id = (String) session.getAttribute("id");
+        pinvo.setMembers_id(id);
+        pinvo.setPno(1);
+        pinvo.setNickname("zcdv");
+        pinvo.setStatus('A');
+        parcelDao.parcelInsert(pinvo);
+        return "redirect:/parcel/pboard";
     }
 }
--- src/main/java/com/nimo/jdbc/dao/ParcelDao.java
+++ src/main/java/com/nimo/jdbc/dao/ParcelDao.java
@@ -21,19 +21,19 @@ public class ParcelDao {
             String query = "select m.nickname, p.pno, p.title, p.content, p.image, m.id  from PARCEL p join MEMBERS m on p.members_id = m.id where p.status=0";
             rs = stmt.executeQuery(query);
 
-            while(rs.next()) {
+            while (rs.next()) {
                 int pno = rs.getInt("pno");
                 String title = rs.getString("title");
                 String content = rs.getString("content");
                 String image = rs.getString("image");
-                if(image==null){
+                if (image == null) {
                     image = "이미지가 없습니다";
                 }
                 char status = 0;
-                String  id = rs.getString("id");
+                String id = rs.getString("id");
                 String nickname = rs.getString("nickname");
 
-                ParcelVo pvo = new ParcelVo(pno,title,content,image,status,id,nickname);
+                ParcelVo pvo = new ParcelVo(pno, title, content, image, status, id, nickname);
                 list.add(pvo);
             }
             Common.close(rs);
@@ -45,6 +45,7 @@ public class ParcelDao {
         }
         return list;
     }
+
     public void parcelInsert(ParcelVo pvo) {
 
         String sql = "insert into PARCEL (pno, title, content, image, status, members_id) values(PARCEL_PNO.nextval,?,?,?,?,?)";
@@ -65,4 +66,55 @@ public class ParcelDao {
         Common.close(pstmt);
         Common.close(conn);
     }
+
+    public void parcelStatusUpdate(int pno) {
+        String sql = "UPDATE PARCEL set status = 1 where pno = ?";
+
+        try {
+            conn = Common.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, pno);
+            pstmt.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Common.close(pstmt);
+        Common.close(conn);
+    }
+
+    public ParcelVo parcelMain(int pnum) {
+        ParcelVo pvo = new ParcelVo();
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            String query = "select m.nickname, p.pno, p.title, p.content, p.image, m.id  from PARCEL p join MEMBERS m on p.members_id = m.id where p.pno= " + pnum;
+            rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                int pno = rs.getInt("pno");
+                String title = rs.getString("title");
+                String content = rs.getString("content");
+                String image = rs.getString("image");
+                if (image == null) {
+                    image = "이미지가 없습니다";
+                }
+                char status = 0;
+                String id = rs.getString("id");
+                String nickname = rs.getString("nickname");
+
+                pvo = new ParcelVo(pno, title, content, image, status, id, nickname);
+            }
+
+            Common.close(rs);
+            Common.close(stmt);
+            Common.close(conn);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return pvo;
+    }
+
+
+
 }
